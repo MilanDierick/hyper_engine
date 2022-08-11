407f21a630c0471f2c83e9f6ed48d9318aa297c1
Milan Dierick <milan.dierick@gmail.com>
2022-08-11 03:06:46 +0200

    Added Windows .bat file to compile all targets

---
+++ build_windows.bat
@@ -0,0 +1,21 @@
+@echo off
+@break off
+@cls
+
+rmdir /S /Q %~dp0build\
+mkdir %~dp0build\
+
+cmake -DCMAKE_BUILD_TYPE=Debug -G Ninja -S %~dp0 -B %~dp0build\cmake-build-debug
+cmake --build %~dp0build\cmake-build-debug --target playground -j 32
+
+cmake -DCMAKE_BUILD_TYPE=Release -G Ninja -S %~dp0 -B %~dp0build\cmake-build-release
+cmake --build %~dp0build\cmake-build-release --target playground -j 32
+
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -G Ninja -S %~dp0 -B %~dp0build\cmake-build-relwithdebinfo
+cmake --build %~dp0build\cmake-build-relwithdebinfo --target playground -j 32
+
+cmake -DCMAKE_BUILD_TYPE=MinSizeRel -G Ninja -S %~dp0 -B %~dp0build\cmake-build-minsizerel
+cmake --build %~dp0build\cmake-build-minsizerel --target playground -j 32
+
+
+if NOT ["%errorlevel%"]==["0"] pause
